4568a5362a7a5539ef0b1f1b177a5054e1736cb6
PranjalShridhar316 <pranjalshridhar9259@gmail.com>
2026-07-23 11:40:44 +0530

    Time: 33 ms (58.36%), Space: 42.9 MB (46.71%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -13,13 +13,11 @@ class Solution:
         if not head or not head.next:
             return True
 
-        # Step 1: Find middle
         slow, fast = head, head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
 
-        # Step 2: Reverse second half
         prev = None
         while slow:
             nxt = slow.next
@@ -27,7 +25,6 @@ class Solution:
             prev = slow
             slow = nxt
 
-        # Step 3: Compare halves
         left, right = head, prev
         while right:
             if left.val != right.val:
